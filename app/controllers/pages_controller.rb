e28ae6505512c182fcab11035c4c20bf13d8fad8
Rails Udemy Course <railsudemycourse@gmail.com>
2016-04-22 19:29:48 +0000

    Style homepage and add links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,10 @@
 class PagesController < ApplicationController
   
-  def index
-    
-  end
-  
   def home
     
   end
   
   def about
-    
-  end
-  
-  def blog
-    
-  end
-  
-  def archive
     
   end
   
